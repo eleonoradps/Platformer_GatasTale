6e753c7626190b287f5ba2d9c33bd12caa20f8f6
L <eleonora.dups@gmail.com>
2019-11-22 17:08:53 +0100

    added gem script done

---
+++ Assets/Scripts/Objects/Gem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    [SerializeField] [Range(0, 5)] private float floatingDistance = 1;
+    [SerializeField] private int directionY = 1;
+    [SerializeField] private Vector3 originalPosition;
+
+    [SerializeField] private AnimationCurve animationCurve;
+
+    [SerializeField] private float offsetY;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    
+    void Update()
+    {
+        offsetY = animationCurve.Evaluate(Time.time);
+        transform.position = new Vector3(originalPosition.x, originalPosition.y + offsetY * floatingDistance);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.GetComponent<PlayerController>();
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(originalPosition == Vector3.zero)
+        {
+            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y + floatingDistance));
+            Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - floatingDistance));
+        }
+        else
+        {
+            Gizmos.DrawLine(originalPosition, new Vector2(originalPosition.x, originalPosition.y + floatingDistance));
+            Gizmos.DrawLine(originalPosition, new Vector2(originalPosition.x, originalPosition.y - floatingDistance));
+        }
+    }
+}
