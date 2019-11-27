52ec787fbfd99b29a61fc72918b6e5b712fa5b0a
L <eleonora.dups@gmail.com>
2019-11-27 15:03:07 +0100

    changed gem sprites

---
+++ Assets/Scripts/Camera/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Transform parallax;
+    [SerializeField] private float speedCoefficient;
+    [SerializeField] private Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = parallax.position;
+    }
+
+    
+    void Update()
+    {
+        transform.position = (new Vector2(speedCoefficient * Time.time + Camera.main.transform.position.x, transform.position.y));
+        lastPosition = parallax.position;
+    }
+}
