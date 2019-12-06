af917a91fc3090ec681254afee3b214351dffd59
L <eleonora.dups@gmail.com>
2019-12-06 16:30:09 +0100

    modified gate animation & started pause menu

--- Assets/Scripts/Gate/Gate.cs
+++ Assets/Scripts/Gate/Gate.cs
@@ -6,7 +6,7 @@ public class Gate : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Gate"))
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
 
         }
