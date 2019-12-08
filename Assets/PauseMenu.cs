ebd55e1a7e32a5a93ea416a08fa6c2444c839595
L <eleonora.dups@gmail.com>
2019-12-08 17:41:32 +0100

    pause menu done

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool pausedGame = false;
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(pausedGame)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        pausedGame = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        pausedGame = true;
+    }
+
+    public void QuitGame()
+    {
+        //Debug.Log("Quit game");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
